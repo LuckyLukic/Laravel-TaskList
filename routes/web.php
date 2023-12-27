1c02c2cb2b272464a3fc83e897501735d6be0411
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-12-27 19:15:37 +0100

    sending data through task form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -23,11 +24,19 @@ Route::get('/tasks', function () {
     return view('index', ['tasks' => Task::latest()->get()]); // latest() returns an instance of the query builder, with an SQL ORDER BY clause applied.
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('tasks.create'); //renderizzo solo la pagina, non applico nessun metodo CRUD
+
 Route::get('/tasks/{id}', function ($id) {
 
     return view('show', ['task' => Task::findOrFail($id)]); //if does not find nythinf return a 404 page
 
 })->name('tasks.show');
+
+route::post('/tasks', function (Request $request) { // Request give us access to all the data sent
+    dd($request->all());
+})->name('tasks.store');
+
+
 
 
 Route::fallback(function () {
